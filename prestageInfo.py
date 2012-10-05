073e9a2b7d21a43fcce24d0a2b971dbf7aae1c80
Nate Walck <nate.walck@gmail.com>
2012-10-05 12:00:48 -0400

    Initial commit of prestageInfo script.  Rather rouh

---
+++ prestageInfo.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+"""
+To Do:
+1. Add better error checking along the way
+2. Fix postToDeployStudio so that it ses the primary key setting instead of being hardcoded
+"""
+
+import os
+import urllib2
+import plistlib
+import optparse
+
+
+def main():
+    """Main"""
+    p = optparse.OptionParser()
+    p.set_usage("""Usage: %prog [options]""")
+    p.add_option('--computername', dest='computerName',
+                 help="""Sets the computer name.""")
+    p.add_option('--hostname', dest='hostName',
+                 help="""Sets the host name.""")
+
+    options, arguments = p.parse_args()
+    if not options.computerName or not options.hostName:
+        p.error("incorrect number of arguments")
+
+    # Save options to proper variables
+    computerName = options.computerName
+    hostName = options.hostName
+
+    # Get the URL for the DS Server from the runtime plist
+    os.system("/usr/bin/plutil -convert xml1 /Library/Preferences/com.deploystudio.server.plist -o /tmp/com.deploystudio.server.plist")
+    dsServerURL = plistlib.readPlist("/tmp/com.deploystudio.server.plist")["server"]["url"]
+
+    # Get primary key setting from DS Server
+    dsServerInfo = plistlib.readPlistFromString(urllib2.urlopen(urllib2.Request(dsServerURL + "/server/get/info")).read())
+    dsPrimaryKey = dsServerInfo["computer_primary_key"]
+
+    # Get serial # of the machine
+    global serialNumber
+    serialNumber = os.environ['DS_SERIAL_NUMBER']
+
+    # Build the settings plist
+    computerPlist = {}
+    computerPlist['dstudio-host-primary-key'] = dsPrimaryKey
+    computerPlist['dstudio-host-serial-number'] = serialNumber
+    computerPlist['cn'] = computerName
+    computerPlist['dstudio-hostname'] = hostName
+
+    postToDeployStudio(dsServerURL, computerPlist)
+
+
+def postToDeployStudio(repoUrl, plistToPost):
+    convertedPlist = plistlib.writePlistToString(plistToPost)
+    dsAuthFile = open("/tmp/DSNetworkRepository/DeployStudioAuthorization", 'r')
+    authString = dsAuthFile.read()
+    # Fix this to use the proper identifier based upon dstudio-host-primary-key
+    submitRequest = urllib2.Request(repoUrl + "/computers/set/entry?id=" + plistToPost['dstudio-host-serial-number'],
+                                    convertedPlist, {'Content-type': 'text/xml'})
+    submitRequest.add_header("Authorization", "Basic %s" % authString)
+    urllib2.urlopen(submitRequest)
+
+if __name__ == '__main__':
+    main()
